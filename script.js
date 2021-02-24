958c291050e073a34b51d664f2203d0b2e8d8c87
Parker Cohen <pfc1025@gmail.com>
2021-02-24 13:30:11 -0600

    Error-Handling Fix

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 'use-strict';
 
+let moonDescription = document.querySelector('.moonDescription');
 let moonText = document.querySelector('.moonText');
 let moonIllumination = document.querySelector('.moonIllumination');
 let moonIcon = document.querySelector('.moonIcon');
+let locationContainer = document.querySelector('.location');
+let locationText = document.querySelector('.locationText');
 let locationTimezone = document.querySelector('.locationTimezone');
 
 let latitude, longitude;
@@ -10,7 +13,9 @@ let latitude, longitude;
 //Geolocation API
 const getPosition = function () {
     return new Promise(function (resolve, reject) {
-        navigator.geolocation.getCurrentPosition(resolve, reject);
+        navigator.geolocation.getCurrentPosition(resolve, err => {
+            alert(`${err.message}: Please enable location services for your browser`);
+        });
     });
 };
 
@@ -23,18 +28,20 @@ const displayPosition = async function () {
 
         //Reverse geocoding API
         const responseGeo = await fetch(`https://geocode.xyz/${latitude},${longitude}?geoit=json`);
-        if (!responseGeo.ok) throw new Error('Could not retrieve city and state data');
+        if (!responseGeo.ok) throw new Error('Could not retrieve location. Reloading page now...');
 
         const dataGeo = await responseGeo.json();
-        console.log(dataGeo);
 
         let city = dataGeo.city.toLowerCase();
+        locationContainer.classList.remove('hidden');
+        locationText.textContent = `Your Current Location:`;
         locationTimezone.textContent = `${city[0].toUpperCase() + city.substring(1)}, ${
             dataGeo.statename
         }`;
     } catch (err) {
         console.error(err);
-        throw err;
+        alert(err);
+        location.reload();
     }
 };
 
@@ -49,13 +56,15 @@ const getMoonPhase = async function () {
         const responseMoon = await fetch(
             `https://api.weatherapi.com/v1/astronomy.json?key=61e062c61e5149e1a5b01136212302&q=${latitude},${longitude}`
         );
-        if (!responseMoon.ok) throw new Error('Could not retrieve current moon phase');
+        if (!responseMoon.ok)
+            throw new Error('Could not retrieve current moon phase. Sever experiencing issues');
 
         const dataMoon = await responseMoon.json();
 
         let moonPhase = String(dataMoon.astronomy.astro.moon_phase);
         let moonPhaseJoined = moonPhase.replace(' ', '_');
 
+        moonDescription.classList.remove('hidden');
         moonText.textContent = moonPhase;
         moonIllumination.textContent = `${dataMoon.astronomy.astro.moon_illumination}% Illumination`;
 
@@ -63,11 +72,10 @@ const getMoonPhase = async function () {
             'beforeend',
             `<img src="img/${moonPhaseJoined}.png" alt="${moonPhase} Created by MarkieAnn Packer from Noun Project">`
         );
-
-        console.log(dataMoon);
     } catch (err) {
         console.error(err);
-        throw err;
+        alert(err);
+        location.reload();
     }
 };
 
